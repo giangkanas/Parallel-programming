efc997703485865f3b3754a6fd456817ff6d691f
giangkanas <118474076+giangkanas@users.noreply.github.com>
2022-11-27 11:06:56 +0200

    Update task_1.c

--- K-Means/task_1.c
+++ K-Means/task_1.c
@@ -1,1 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 10
+#define Nv 5
+#define Nc 3
+
+
+
+float Vec[N][Nv];			
+float Center[Nc][Nv];
+int Classes[N];
+
+
+
+void createVector(void){	
+	//TODO...
+}
+
+
+void initiallizeCenters(void){
+	//TODO...
+}
+
+
+void Classification(void){
+	//TODO...
+}
+
+
+void EstimateCenters(void){
+	//TODO...	
+}
+
+
+int Terminate(){
+	//TODO...
+}
+
+//#######################################################################
+
+int main(int argc, char *argv[]) {
+	
+	createVector();
+	initiallizeCenters();
+	
+	do{
+		Classification();
+		EstimateCenters();
+	}while(Terminate());
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
 
